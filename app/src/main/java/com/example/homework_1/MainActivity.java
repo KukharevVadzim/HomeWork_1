0f8a8066c08b28c75d8a313a3128811620e9063d
KukharevVadzim <79867306+KukharevVadzim@users.noreply.github.com>
2021-03-24 17:02:32 +0300

    Revert "Work with variables"

--- app/src/main/java/com/example/homework_1/MainActivity.java
+++ app/src/main/java/com/example/homework_1/MainActivity.java
@@ -4,19 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity<b> extends AppCompatActivity {
-    int a = 10;
-    byte b = 125;
-    short c = 3000;
-    long d = 300000000;
-    float f = 30.467f;
-    double g = 3.14567;
-    char h = 'f';
-    boolean k = true;
+public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 }
-
