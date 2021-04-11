84e83f7b289893ebf0bd2a5f355a0ba658145bfc
KukharevVadzim <kukharev.vadzim@gmail.com>
2021-04-11 12:39:12 +0300

    task_13

--- app/src/main/java/com/example/task_9/MainActivity.java
+++ app/src/main/java/com/example/task_9/MainActivity.java
@@ -1,21 +1,20 @@
 package com.example.task_9;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
-import android.media.browse.MediaBrowser;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
-import com.example.task_9.R;
-import com.example.task_9.SecondActivity;
-
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.text.BreakIterator;
+import java.util.LinkedList;
+import java.util.concurrent.Callable;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     static String MAIN_ACTIVITY_DATA = "MAIN_ACTIVITY_DATA";
     private EditText editText;
     public Button buttonCreateFile;
+    public Button buttonResult;
+    public TextView textViewResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         };
+
         buttonCreateFile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -60,10 +62,60 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent5);
             }
         });
-        click.setOnClickListener(listener);
+
+        buttonResult = findViewById(R.id.buttonResult);
+        buttonResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textViewResult = findViewById(R.id.textViewResult);
+                Callable callable = getDataFromCallable();
+                FutureTask futureTask = new FutureTask(callable);
+                new Thread(futureTask).start();
+                try {
+                    textViewResult.setText(futureTask.get().toString());
+                } catch (ExecutionException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
     }
 
-    private void initThreadClick() {
+    public Callable<Integer> getDataFromCallable() {
+        Callable<Integer> callable = new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                int summa = 0;
+                for (int i = 2; i <= Integer.parseInt(editText.getText().toString()); i++) {
+                    summa += i;
+                }
+                return summa;
+            }
+        };
+        return callable;
+    }
+
+    public void ArrayWithLinkedList() {
+        ArrayList<Double> arrayList = new ArrayList<>();
+        LinkedList<Double> linkedList = new LinkedList<>();
+        int a = 1000000;
+        int b = 1000;
+        long time = System.currentTimeMillis();
+        for (int i = 0; i < a; i++) {
+            arrayList.add(Math.random());
+            linkedList.add(Math.random());
+        }
+        for (int i = 0; i < b; i++) {
+            arrayList.get((int) (Math.random() * a - 1));
+        }
+        System.out.println(System.currentTimeMillis() - time);
+        for (int i = 0; i < b; i++) {
+            linkedList.get((int) (Math.random() * a - 1));
+        }
+        System.out.println(System.currentTimeMillis() - time);
+    }
+
+    public void initThreadClick() {
         Button click = findViewById(R.id.clickTest);
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
@@ -75,39 +127,41 @@ public class MainActivity extends AppCompatActivity {
             }
         };
     }
-    private void initToastClicK() {
-          Button button = findViewById(R.id.btn);
-          button.setOnClickListener(new View.OnClickListener() {
-              @Override
-              public void onClick(View v) {
-                  Toast.makeText(v.getContext(), "Messeg" + thread1.getState(), Toast.LENGTH_LONG).show();
-              }
-          });
+
+    public void initToastClicK() {
+        Button button = findViewById(R.id.btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "Messeg" + thread1.getState(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
-                public void Methods() {
-                    Person Vadim = new Person(65, 176, 26);
-                    Person Kolia = new Person(76, 177, 24);
-                    Person Lesha = new Person(89, 198, 27);
-                    Person Dima = new Person(65, 176, 26);
-                    System.out.println("Equals1 " + Vadim.equals(Dima));
-                    System.out.println("Equals2 " + Kolia.equals(Lesha));
-                    System.out.println("ToString1: " + Vadim.toString());
-                    System.out.println("ToString1: " + Lesha.toString());
-                    int hashCode;
-                    hashCode = Kolia.hashCode();
-                    System.out.println("Hashcode1 Vadim: " + hashCode);
-                    System.out.println("Hashcode2 Kolia: " + hashCode);
-                }
 
-                public void methodClone() throws CloneNotSupportedException {
-                    Person Alina = new Person(70, 175, 24);
-                    Person copyBox = (Person) Alina.clone();
-                    System.out.println("Copy Person" + copyBox);
-                }
-
-                public void Strings() {
-                    String str = "Android Developer";
-                    String reverse = new StringBuffer(str).reverse().toString();
-                    System.out.println("Строка в обратном порядке, после реверса: " + reverse);
-                }
-            };
+    public void Strings() {
+        String str = "Android Developer";
+        String reverse = new StringBuffer(str).reverse().toString();
+        System.out.println("Строка в обратном порядке, после реверса: " + reverse);
+    }
+}
+//    public void Methods() {
+//        Person Vadim = new Person(65, 176, 26);
+//        Person Kolia = new Person(76, 177, 24);
+//        Person Lesha = new Person(89, 198, 27);
+//        Person Dima = new Person(65, 176, 26);
+//        System.out.println("Equals1 " + Vadim.equals(Dima));
+//        System.out.println("Equals2 " + Kolia.equals(Lesha));
+//        System.out.println("ToString1: " + Vadim.toString());
+//        System.out.println("ToString1: " + Lesha.toString());
+//        int hashCode;
+//        hashCode = Kolia.hashCode();
+//        System.out.println("Hashcode1 Vadim: " + hashCode);
+//        System.out.println("Hashcode2 Kolia: " + hashCode);
+//    }
+//
+//    public void methodClone() throws CloneNotSupportedException {
+//        Person Alina = new Person(70, 175, 24);
+//        Person copyBox = (Person) Alina.clone();
+//        System.out.println("Copy Person" + copyBox);
+//    }
+//}
